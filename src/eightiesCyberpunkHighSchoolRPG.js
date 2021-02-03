f3d5fa5029d88c1b7e5f118341da624697a6bf81
Eric Endsley <eric.endsley4@gmail.com>
2021-02-03 13:50:39 -0800

    add functionality to modify a chars stat
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>

--- src/eightiesCyberpunkHighSchoolRPG.js
+++ src/eightiesCyberpunkHighSchoolRPG.js
@@ -1,8 +1,8 @@
 
 export const storeState = (initialState = {}) => {
   let currentState = initialState;
-  return (stateChangeFunction = (state) => state) => {
-    const newState = stateChangeFunction(currentState);
+  return (stateChangeFunction = state => state, name) => {
+    const newState = stateChangeFunction(currentState, name);
     currentState = { ...newState };
     return newState;
   };
@@ -19,28 +19,51 @@ export const addCharacterToState = (charInitialObject) => {
 
 export const changeState = (prop) => { // basic numerical state update
   return(value) => {
+    return (state, name) => ({
+      ...state,
+      [name]: { ...state[name], [prop]: (state[name][prop] || 0) + value }
+    }); 
+  };
+};
+
+export const gainAbility = (prop) => { // basic numerical state update
+  return(value) => {
     return (state) => ({
       ...state,
-      [prop]: (state[prop] || 0) + value
+      [prop]: value
     });
   };
 };
 
+const canCallTheCops = (char) => ({
+  callTheCops: () => {
+    return `${char} calls the cops`;
+  }
+});
 
-// const canCallTheCops = (char) => ({
-//   callTheCops: () => {
-//     return `${char} calls the cops`;
-//   }
-// });
 
-const updateStateObject = storeState();
+
+const charStateControl = storeState();
 
 const evilPrincipalInitialValues = { tech: 2, speed: 2, constitution: 4, authority: 5, looks: 1, principles: 1 };
 
 const joeTheEvilPrincipal = addCharacterToState(evilPrincipalInitialValues)("Joe");
-
-const newState = updateStateObject(joeTheEvilPrincipal);
-
-console.log(newState);
+const helenTheEvilPrincipal = addCharacterToState(evilPrincipalInitialValues)("Helen");
 
 
+const addJoeToState = charStateControl(joeTheEvilPrincipal); // this represents a state that contains Joe
+const addHelenToState = charStateControl(helenTheEvilPrincipal); // this represents a state that contains Joe & Helen
+
+const addOneCon = changeState("constitution")(1)
+
+const helenConUp = charStateControl(addOneCon, "Helen");
+
+const joeNewAbility = gainAbility(canCallTheCops(addJoeToState.name)); // try passing newState.name
+
+const newerState = charStateControl(joeNewAbility);
+
+console.log(addJoeToState);
+console.log(addHelenToState);
+console.log(helenConUp);
+console.log(joeNewAbility);
+console.log(newerState);
