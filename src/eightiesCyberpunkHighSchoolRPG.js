4c384d12d95291921b7f2efcd1e238728a67bb38
Eric Endsley <eric.endsley4@gmail.com>
2021-02-03 16:14:55 -0800

    add basic UI, add janitor char type

--- src/eightiesCyberpunkHighSchoolRPG.js
+++ src/eightiesCyberpunkHighSchoolRPG.js
@@ -27,43 +27,64 @@ export const changeState = (prop) => { // basic numerical state update
 };
 
 export const gainAbility = (prop) => { // basic numerical state update
-  return(value) => {
-    return (state) => ({
+    return (state, name) => ({
       ...state,
-      [prop]: value
+      [name]: { ...state[name], ability: prop }
     });
-  };
 };
 
-const canCallTheCops = (char) => ({
-  callTheCops: () => {
-    return `${char} calls the cops`;
-  }
-});
-
-
+const callTheCops = () => {
+  return `The character calls the cops`;
+}   
 
 const charStateControl = storeState();
 
 const evilPrincipalInitialValues = { tech: 2, speed: 2, constitution: 4, authority: 5, looks: 1, principles: 1 };
+const undervaluedJanitorInitialValues = { tech: 5, speed: 2, constitution: 2, authority: 1, looks: 2, principles: 5 };
 
 const joeTheEvilPrincipal = addCharacterToState(evilPrincipalInitialValues)("Joe");
 const helenTheEvilPrincipal = addCharacterToState(evilPrincipalInitialValues)("Helen");
-
+const xanderTheUndervaluedJanitor = addCharacterToState(undervaluedJanitorInitialValues)("Xander");
 
 const addJoeToState = charStateControl(joeTheEvilPrincipal); // this represents a state that contains Joe
 const addHelenToState = charStateControl(helenTheEvilPrincipal); // this represents a state that contains Joe & Helen
+const addXanderToState = charStateControl(xanderTheUndervaluedJanitor);
 
 const addOneCon = changeState("constitution")(1)
+// const addOneSpeed = changeState("speed")(1)
+// const addOneTech = changeState("tech")(1)
+// const addOneAuth = changeState("authority")(1)
+// const addOneLooks = changeState("looks")(1)
+// const addOnePrinc = changeState("principles")(1)
+// const minusOneCon = changeState("constitution")(-1)
+const minusOneSpeed = changeState("speed")(-1)
+const minusOneTech = changeState("tech")(-1)
+// const minusOneAuth = changeState("authority")(-1)
+// const minusOneLooks = changeState("constitution")(-1)
+// const minusOnePrinc = changeState("principles")(-1)
 
 const helenConUp = charStateControl(addOneCon, "Helen");
 
-const joeNewAbility = gainAbility(canCallTheCops(addJoeToState.name)); // try passing newState.name
+const gainCallTheCops = gainAbility(callTheCops); // try passing newState.name
 
-const newerState = charStateControl(joeNewAbility);
+const newerState = charStateControl(gainCallTheCops, "Joe");
 
 console.log(addJoeToState);
 console.log(addHelenToState);
+console.log(addXanderToState);
 console.log(helenConUp);
-console.log(joeNewAbility);
-console.log(newerState);
+console.log(gainCallTheCops);
+console.log(newerState.Joe.ability());
+
+// User Interface //
+
+$(document).ready(function() {
+
+  $('#mutual-attack').click(function() {
+    const joe = charStateControl(minusOneSpeed, "Joe");
+    const xander = charStateControl(minusOneTech, "Xander");
+    
+    $('#joe-stats').text(`Joe's speed is now: ${joe.Joe.speed}`);
+    $('#xander-stats').text(`Xander's tech is now: ${xander.Xander.tech}`);
+  });
+});
